a96b766de0982761b7eee6b3b1d39545a381180b
Brandon Au <tech.brandonau@gmail.com>
2021-03-14 15:23:03 -0400

    feat: create POST endpoint to create a material

--- api/api.js
+++ api/api.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
 const apiServer = express();
 const port = 3000;
-
 const basePath = '/materials'
 
 const materials = [];
 
 apiServer.get(basePath, (request, response) => {
 	response.send(materials);
+});
+
+apiServer.post(basePath, (request, response) => {
+	const newMaterial = {
+		...request,
+		id: uuidv4()
+	}
+
+	materials.push(newMaterial);
+
+	response.status(201).send(newMaterial);
 });
 
 apiServer.listen(port, () => {
