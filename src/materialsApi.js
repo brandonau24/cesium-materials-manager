30bf9dd2e79bf8459bca58ac86512742b8582183
Brandon Au <tech.brandonau@gmail.com>
2021-03-16 19:45:42 -0400

    fix: fix API not returning promise from axios call

--- src/materialsApi.js
+++ src/materialsApi.js
@@ -3,16 +3,11 @@ import axios from 'axios';
 const baseUrl = 'http://localhost:3000/materials';
 
 const materialsApi = {
-	addMaterial: (material) => {
-		axios.post(baseUrl, material)
-			.then((response) => response.data);
-	},
+	addMaterial: (material) => axios.post(baseUrl, material).then((response) => response.data),
 	deleteMaterial: (materialId) => {
 		axios.delete(`${baseUrl}/${materialId}`).then((response) => response);
 	},
-	modifyMaterial: (material) => {
-		axios.put(`${baseUrl}/${material.id}`).then((response) => response);
-	},
+	modifyMaterial: (material) => axios.put(`${baseUrl}/${material.id}`).then((response) => response),
 	getMaterials: () => {
 		axios.get(baseUrl).then((response) => response.data);
 	}
