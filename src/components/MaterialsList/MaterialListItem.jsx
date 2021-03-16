a692d2f8b9b88a241bf9c988fe737c17797bb588
Brandon Au <tech.brandonau@gmail.com>
2021-03-15 21:59:21 -0400

    chore: add prop-types checking to material list item component

--- src/components/MaterialsList/MaterialListItem.jsx
+++ src/components/MaterialsList/MaterialListItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // eslint-disable-next-line jsx-a11y/control-has-associated-label
 const MaterialListItem = ({ material }) => (
@@ -6,5 +7,16 @@ const MaterialListItem = ({ material }) => (
 		<div className="circle" data-testid="material-color" style={{ color: material.color }} />
 	</button>
 );
+
+MaterialListItem.propTypes = {
+	material: PropTypes.shape({
+		id: PropTypes.string,
+		name: PropTypes.string,
+		color: PropTypes.string,
+		costPerCubicMeter: PropTypes.number,
+		volume: PropTypes.number,
+		deliveryDate: PropTypes.string
+	}).isRequired
+};
 
 export default MaterialListItem;
