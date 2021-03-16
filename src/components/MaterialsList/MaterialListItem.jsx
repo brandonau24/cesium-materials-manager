44dc8c962aa3d1f4339fd6826fc7fbc876333240
Brandon Au <tech.brandonau@gmail.com>
2021-03-15 21:45:11 -0400

    feat: show material color in material list item component

--- src/components/MaterialsList/MaterialListItem.jsx
+++ src/components/MaterialsList/MaterialListItem.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 // eslint-disable-next-line jsx-a11y/control-has-associated-label
-const MaterialListItem = () => <button type="button" />;
+const MaterialListItem = ({ material }) => (
+	<button type="button">
+		<div className="circle" data-testid="material-color" style={{ color: material.color }} />
+	</button>
+);
 
 export default MaterialListItem;
---
+++ src/components/MaterialsList/__tests__/MaterialListItem.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import MaterialListItem from 'components/MaterialsList/MaterialListItem';
+
+test('renders color of material', () => {
+	const material = {
+		name: 'Gravel',
+		color: 'gray',
+		costPerCubicMeter: 0.25,
+		volume: 1000,
+		deliveryDate: '2021-3-15'
+	};
+
+	render(<MaterialListItem material={material} />);
+
+	const materialColorElement = screen.getByTestId('material-color');
+	expect(materialColorElement).toHaveStyle('color: gray');
+	expect(materialColorElement).toHaveClass('circle');
+});
