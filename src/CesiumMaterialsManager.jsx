fd4e88efe6375b6873dce142ddc7aa417fe5f9b7
Brandon Au <tech.brandonau@gmail.com>
2021-03-15 20:40:11 -0400

    test: add test to show a cost of 0 when there are no materials

--- src/CesiumMaterialsManager.jsx
+++ src/CesiumMaterialsManager.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+
 import MaterialsList from 'components/MaterialsList/MaterialsList';
 
 import './CesiumMaterialsManager.scss';
 
-const CesiumMaterialsManager = () => (
-	<div id="cesium-materials-manager">
-		<h2>Materials</h2>
-		<MaterialsList />
-	</div>
+const totalMaterialsCostSelector = createSelector(
+	(state) => state.materials,
+	(materials) => {
+		const totalMaterialsCost = materials.reduce(
+			(totalCost, material) => totalCost + (material.volume * material.costPerCubicMeter),
+			0
+		);
+
+		return totalMaterialsCost.toFixed(2);
+	}
 );
+
+const CesiumMaterialsManager = () => {
+	const totalMaterialsCost = useSelector(totalMaterialsCostSelector);
+
+	return (
+		<div id="cesium-materials-manager">
+			<h2>Materials</h2>
+			<MaterialsList />
+			<div className="__total-materials-cost">{`$${totalMaterialsCost}`}</div>
+		</div>
+	);
+};
 
 export default CesiumMaterialsManager;
--- src/__tests__/CesiumMaterialsManager.test.jsx
+++ src/__tests__/CesiumMaterialsManager.test.jsx
@@ -3,9 +3,15 @@ import { render, screen } from 'test-utils';
 import CesiumMaterialsManager from '../CesiumMaterialsManager';
 
 test('renders Materials header', () => {
-	render(<CesiumMaterialsManager />);
+	render(<CesiumMaterialsManager />, { initialState: { materials: [] } });
 
 	const materialsHeader = screen.getByRole('heading', { level: 2 });
 
 	expect(materialsHeader).toHaveTextContent('Materials');
+});
+
+test('display a cost of 0 when there are no materials', () => {
+	render(<CesiumMaterialsManager />, { initialState: { materials: [] } });
+
+	expect(screen.getByText(/\$0\.00/)).toBeVisible();
 });
